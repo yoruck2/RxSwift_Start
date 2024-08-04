bf62403ae56b8145208ffd7775262bbdab89a9b0
yoruck2 <yoruck2@gmail.com>
2024-08-04 23:57:45 +0900

    💄 Phrase에 메세지 추가

--- RxSwift_Start/Resource/Constants.swift
+++ RxSwift_Start/Resource/Constants.swift
@@ -15,4 +15,12 @@ enum Color {
 enum Phrase {
     static let validAge = "가입 가능한 나이입니다."
     static let invalidAge = "만 17세 이상만 가입 가능합니다."
+    
+    static let validEmail = "가능한 이메일 입니다!"
+    static let duplicatedEmail = "이미 가입된 이메일 입니다."
+    static let invalidEmail = "이메일 형식에 맞게 입력해주세요."
+    
+    static let invalidBirthday = "만 17세 이상만 가입 가능합니다."
+    
+    static let signUpComplete = "회원가입이 완료 되었습니다!\n환영합니다!"
 }
