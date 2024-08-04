3109c9aa9b9b8e543f72fa5e8fb2f928f09de800
yoruck2 <yoruck2@gmail.com>
2024-08-04 23:58:14 +0900

    ✨ ShoppingItem 모델 생성

--- RxSwift_Start/ShoppingList/ShoppingItem.swift
+++ RxSwift_Start/ShoppingList/ShoppingItem.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct ShoppingItem {
+    var name: String
+    var isDone: Bool
+    var isFavorite: Bool
+    
+    init(name: String, isDone: Bool = false, isFavorite: Bool = false) {
+        self.name = name
+        self.isDone = isDone
+        self.isFavorite = isFavorite
+    }
+}
