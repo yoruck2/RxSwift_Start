7d682f8cd1f9861e7249bb1985fef31ae382cb51
yoruck2 <yoruck2@gmail.com>
2024-08-05 05:11:08 +0900

    🩹 모델에 id 프로퍼티 추가 중복되는 내용이 있더라도 구별하기 위함

--- RxSwift_Start/ShoppingList/ShoppingItem.swift
+++ RxSwift_Start/ShoppingList/ShoppingItem.swift
@@ -8,11 +8,13 @@
 import Foundation
 
 struct ShoppingItem {
+    var id: UUID
     var name: String
     var isDone: Bool
     var isFavorite: Bool
     
     init(name: String, isDone: Bool = false, isFavorite: Bool = false) {
+        self.id = UUID()
         self.name = name
         self.isDone = isDone
         self.isFavorite = isFavorite
